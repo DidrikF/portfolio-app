e8a64fd228fef9040283275e7a55405e7e33904e
Didrik Fleischer <didrik.fleischer@gmail.com>
2019-09-14 19:34:35 +0200

    Add onChange logic to CSSManager

--- src/CSSManager.js
+++ src/CSSManager.js
@@ -4,15 +4,13 @@ import axios from 'axios'
 import { getId } from './helpers';
 
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
-import { thisExpression } from '@babel/types';
+import { thisExpression, optionalCallExpression } from '@babel/types';
 
 
 const reorder = (list, startIndex, endIndex) => {
     const result = Array.from(list);
     const [removed] = result.splice(startIndex, 1);
     result.splice(endIndex, 0, removed);
-
-
 
     return result;
 };
@@ -49,11 +47,11 @@ class CSSManager extends React.Component {
                     query: "",
                 },
                 {
-                    name: "Tablet", // Mobile first, I guess that makes sence
+                    name: "tablet", // Mobile first, I guess that makes sence
                     query: "@media only screen and (min-width: 600px) ",
                 },
                 {
-                    name: "Desktop",
+                    name: "desktop",
                     query: "@media only screen and (min-width: 1000px) "
                 }
             ],
@@ -65,14 +63,14 @@ class CSSManager extends React.Component {
                 "component",
                 "rich text",
             ],
-            editing: null,
+            editing: null, // The id of the item being edited!
             cssDocument: [ // these are what I want to edit
                 {
                     id: getId(), // GIV IT AN ID ON CREATION?
                     mediaQuery: "", // name
                     selector: "",
-                    scopes: [], // use only by the react app to help the user see only the relevent classes.
-                    attributes: [],
+                    scopes: [], // use only by the react app to help the user see only the relevant classes.
+                    attributes: [], // array of {key: "...", val: "..."}
                 },
                 {
                     id: getId(),
@@ -84,16 +82,15 @@ class CSSManager extends React.Component {
             ],
         }
 
-
         this.createItem = this.createItem.bind(this);
         this.deleteItem = this.deleteItem.bind(this);
-        this.editItem = this.editItem.bind(this);
         this.addAttribute = this.addAttribute.bind(this)
         this.deleteAttribute = this.deleteAttribute.bind(this)
         this.handleScopeChange = this.handleScopeChange.bind(this);
-        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSelectorChange = this.handleSelectorChange.bind(this);
+        this.handleAttributeInputChange = this.handleAttributeInputChange.bind(this);
         this.saveItem = this.saveItem.bind(this);
-        this.onDragEnd = this.onDragEnd.bind(this);
+        this.onDragEnd = this.onDragEnd.bind(this);       
         
     }
 
@@ -114,88 +111,93 @@ class CSSManager extends React.Component {
             return {
                 cssDocument: state.cssDocument
             }
-        })
+        });
     }
 
-    deleteItem(index) {
+    deleteItem(id) { // maybe not rely on the index at all, and do all manipulation based on the ID
         this.setState((state) => {
-            state.cssDocument.splice(index, 1);
+            const indexToDelete = state.cssDocument.findIndex(item => item.id === id);
+            state.cssDocument.splice(indexToDelete, 1);
+            return {
+                cssDocument: state.cssDocument,
+            }
+        });
+    }
+
+    handleScopeChange(e, editingId) {
+        e.persist();
+        console.log("Scope change, select event (how to get a hold of all selected scopes): ", e);
+        let scopes = [];
+        for (let option of e.target.selectedOptions) {
+            scopes.push(option.value);
+        }
+        this.setState(state => {
+            const itemIndex = state.cssDocument.findIndex(item => item.id === editingId);
+            state.cssDocument[itemIndex]["scopes"] = scopes;
             return {
                 cssDocument: state.cssDocument,
             }
         })
     }
 
-    editItem(index) {
-        this.setState((state) => {
-            return {
-                editing: state.cssDocument[index], // edit by reference, the index in the array does not matter here...
-            }
-        })
-    }
-
-    handleScopeChange(e) {
-        console.log("Scope Chage, select event: ", e)
-        this.setState(state => {
-            //state.editing["scopes"] 
-        })
-    }
-
-    handleInputChange(e) {
-        const name = e.tartget.name;
+    handleSelectorChange(e, editingId) {
+        e.persist();
+        const name = e.target.name;
         const value = e.target.value;
-        // may be an attribute or the selecor for the item
-
-
-    }
-
-    addAttribute(index) {
-        if (!index) {
-            this.setState(state => {
-                state.editing["attributes"].push({
-                    key: "",
-                    value: "",
-                })
-                return {
-                    editing: state.editing,
-                }
-            })
-        } else {
-            this.setState(state => {
-                state.editing["attributes"].splice(index, 0, {
-                    key: "",
-                    value: "",
-                })
-                return {
-                    editing: state.editing,
-                }
-            })
-        }
-    }
-
-    deleteAttribute(index) {
         this.setState(state => {
-            state.editing["attributes"].splice(index, 1)
+            const itemIndex = state.cssDocument.findIndex(item => item.id === editingId);
+            state.cssDocument[itemIndex][name] = value;
             return {
-                editing: state.editing,
+                cssDocument: state.cssDocument,
+            };
+        });
+    }
+
+    handleAttributeInputChange(e, index, editingId) {
+        const name = e.target.name;
+        const value = e.target.value;
+        this.setState(state => {
+            const itemIndex = state.cssDocument.findIndex(item => item.id === editingId);
+            console.log("handleAttributeChange: ", name, value);
+            state.cssDocument[itemIndex]["attributes"][index][name] = value
+            return {
+                cssDocument: state.cssDocument,
+            }
+        });
+    }
+
+    addAttribute(attributeIndex, editingId) {
+        this.setState(state => {
+            const itemIndex = state.cssDocument.findIndex(item => item.id === editingId);
+            if (typeof attributeIndex !== "undefined") {
+                state.cssDocument[itemIndex]["attributes"].splice(attributeIndex + 1, 0, {
+                    key: "",
+                    value: "",
+                });
+            } else {    
+                state.cssDocument[itemIndex]["attributes"].push({
+                    key: "",
+                    value: "",
+                });    
+            }
+            return {
+                cssDocument: state.cssDocument,
+            };
+        });
+    }
+
+
+    deleteAttribute(attributeIndex, editingId) {
+        this.setState(state => {
+            const itemIndex = state.cssDocument.findIndex(item => item.id === editingId);
+            state.cssDocument[itemIndex]["attributes"].splice(attributeIndex, 1)
+            return {
+                cssDocument: state.cssDocument,
             }
         })
     }
 
-    saveItem() {
-        // find the matching item by id in the cssDocument
-
-        // The item has been updated by reference as of now, maybe just force an update?
-        /*
-        this.setState(state => {
-            const indexToReplace; 
-        })
-        */
-        this.forceUpdate();
-
-        // replace that item
-
-
+    saveItem() { // why is this even necessary?
         this.writeClassesToStyleElement();
     }   
 
@@ -231,56 +233,6 @@ class CSSManager extends React.Component {
         return (
             <div className="CSSM__container">
                 {/* Show/hide buttons */}
-                
-                { this.state.editing &&
-                    <React.Fragment>
-                        <h4>Selector Editor</h4>
-                        <form className="CSSM__form">
-                            <input 
-                                className="CSSM__selector-input" 
-                                placeholder="Selector" 
-                                value={this.state.editing.selector} 
-                                onChange={this.handleInputChange}
-                                name="selector"
-                            />
-                            
-                            <select multiple className="CSSM__scope-select">
-                                {this.state.scopes.map(scope => {
-                                    return (
-                                        <option value={scope} className="CSSM__scope-option" onSelect={this.handleScopeChange}>
-                                            {scope.charAt(0).toUpperCase()}
-                                        </option>
-                                    )
-                                })}
-                            </select>
-                            <table>
-                                <thead>
-                                    <tr>
-                                        <th>Attribute</th>
-                                        <th>Value</th>
-                                        <th></th>
-                                        <th></th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {this.state.editing.attributes.map((att, index) => {
-                                        return (
-                                            <tr key={index}>
-                                                <td><input value={att.key} key={// What here, make 2 handlers?} onChange={this.handleInputChange} /></td>
-                                                <td><input value={att.value} key={// WHAT HERE, make 2 handlers} onChange={this.handleInputChange} /></td>
-                                                <td><button onClick={() => this.deleteAttribute(index)}>Delete</button></td>
-                                                <td><button onClick={(e) => this.addAttribute(index)}>New</button></td>
-                                            </tr>
-                                        )
-                                    })}
-                                </tbody>
-                            </table>
-                            <button onClick={(e) => {e.preventDefault(); this.addAttribute()}}>New Attribute</button>
-                            <button onClick={(e) => {e.preventDefault(); this.saveItem()}}>Save</button>
-                        </form>
-                    </React.Fragment>
-                }
-
                 <div>
                     <h3>CSS Doc</h3>
                     <DragDropContext onDragEnd={this.onDragEnd}>
@@ -307,9 +259,59 @@ class CSSManager extends React.Component {
                                                         <p className="CSSM__item-info">Selector: {item.selector}</p> 
                                                         <p className="CSSM__item-info">Scopes: {item.scopes.join(", ")}</p>
                                                     </div>
+                                                        <React.Fragment>
+                                                            <h4>Selector Editor</h4>
+                                                            <form className="CSSM__form" onSubmit={(e) => e.preventDefault()}>
+                                                                <input 
+                                                                    className="CSSM__selector-input" 
+                                                                    placeholder="Selector" 
+                                                                    name="selector"
+                                                                    value={item.selector} 
+                                                                    itemid={item.id}
+                                                                    onChange={(e) => this.handleSelectorChange(e, item.id)}
+                                                                />
+                                                                
+                                                                <select multiple className="CSSM__scope-select" name="scope" onChange={this.handleScopeChange}>
+                                                                    {this.state.scopes.map(scope => {
+                                                                        return (
+                                                                            <option 
+                                                                                className="CSSM__scope-option" 
+                                                                                itemid={item.id} 
+                                                                                value={scope} 
+                                                                            >
+                                                                                {scope.charAt(0).toUpperCase() + scope.slice(1)}
+                                                                            </option>
+                                                                        )
+                                                                    })}
+                                                                </select>
+                                                                <table>
+                                                                    <thead>
+                                                                        <tr>
+                                                                            <th>Attribute</th>
+                                                                            <th>Value</th>
+                                                                            <th></th>
+                                                                            <th></th>
+                                                                        </tr>
+                                                                    </thead>
+                                                                    <tbody>
+                                                                        {item.attributes.map((att, index) => {
+                                                                            return (
+                                                                                <tr key={index}>
+                                                                                    <td><input name="key" value={att.key} onChange={(e) => this.handleAttributeInputChange(e, index, item.id)}/></td>
+                                                                                    <td><input name="value" value={att.value} onChange={(e) => this.handleAttributeInputChange(e, index, item.id)}/></td>
+                                                                                    <td><button onClick={(e) => { e.preventDefault(); this.deleteAttribute(index, item.id)}}>Delete</button></td>
+                                                                                    <td><button onClick={(e) => { e.preventDefault(); this.addAttribute(index, item.id)}}>New</button></td>
+                                                                                </tr>
+                                                                            )
+                                                                        })}
+                                                                    </tbody>
+                                                                </table>
+                                                                <button onClick={(e) => { e.preventDefault(); this.addAttribute(undefined, item.id)}}>New Attribute</button>
+                                                                <button onClick={(e) => { e.preventDefault(); this.saveItem(item.id)}}>Save</button>
+                                                            </form>
+                                                        </React.Fragment>
                                                     <div className="CSSM__item-container-right">
-                                                        <button className="CSSM__button-edit" onClick={this.editItem}>Edit</button>
-                                                        <button className="CSSM__button-delete" onClick={this.deleteItem}>Delete</button>
+                                                        <button className="CSSM__button-delete" onClick={() => this.deleteItem(item.id)}>Delete</button>
                                                     </div>
                                                 </div>
                                             )}
